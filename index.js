9a191406966845ad334fa010fa912db8eae8281a
Jason Kelly <jkelly09@gmail.com>
2015-02-19 14:14:51 -0800

    add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
